0b5ca7f059974da65a8555dfca71acfca1daaf56
ghoshpoulami1293 <ghoshpoulami1293@gmail.com>
2023-11-15 13:21:54 -0500

    practice prac 3

--- Classwork15Practice/ArrayHeap.java
+++ Classwork15Practice/ArrayHeap.java
@@ -42,8 +42,35 @@ public class ArrayHeap implements Heap{
 
     @Override
     public int remove() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'remove'");
+        int value = array[0];       // highest priority element to be removed
+        size--;                     // decrementing size for the operation remove , this can also be used as an index.
+
+        array[0]= array[size];      // move element from rightmost bottom(last index) to index 0
+        array[size] = 0;            // last index element = 0
+
+        siftDown(array, size);      // to sift down the element from the parent to the child down the tree
+        return value;               // return element with highest priority
+    }
+
+    private void siftDown(int[] array, int size) {
+        int childIndex = 0;
+        int parentIndex;
+
+        do{
+            parentIndex = childIndex;                                    //parent is now the child- initialize to sift down
+            
+            int leftChild = (parentIndex * 2)+1;                         //calculate left child
+            int rightChild = leftChild + 1;                              //calculate right child
+
+            if(leftChild<size && array[leftChild] < array[childIndex]){  //if left child is within last index and less than present parent
+                childIndex = leftChild;                                  //set child index as left child
+            }
+
+            if(rightChild<size && array[rightChild] < array[childIndex]){//if right child is within last index and less than present parent
+                childIndex = rightChild;                                 //set child index as right child
+            }
+            swap(array,  parentIndex, childIndex);                       //swap parent and child 
+        }while(parentIndex!=childIndex);
     }
 
     @Override
@@ -51,5 +78,22 @@ public class ArrayHeap implements Heap{
       return size;
     }
 
-    
+    public static void main(String[] args) {
+        Heap heap = new ArrayHeap();
+        heap.add(24);
+        heap.add(60);
+        heap.add(68);
+        heap.add(73);
+        System.out.println(heap);
+        heap.add(10);
+        System.out.println(heap);
+
+        while(heap.size()>0){
+            System.out.println(heap.remove());
+            System.out.println(heap.remove());
+            System.out.println(heap.remove());
+            System.out.println(heap.remove());
+            System.out.println(heap.remove());
+        }
+    }
 }
